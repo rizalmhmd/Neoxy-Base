543dd9c38d1d8d264b0b69136612b7105cf83b38
Lexxy24 <pebrikito0@gmail.com>
2022-06-23 03:40:57 +0700

    neoxymd

---
+++ connect/language/help.js
@@ -0,0 +1,98 @@
+exports.help = (prefix, author, botname, owner) => {
+return `
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+*( BOT WA MULTI DEVICE )*
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+❑ _*MY SERVER BOT INFO*_
+*× Owner Name : ${global.author}*
+*× Bot Name: ${global.botname}*
+*× My Owner : ${global.owner}*
+*× My Team : Neoxy Team*
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+
+*∆ [ SIMPEL BOT MENU ] ∆*
+
+❑ *OTHERS MENU*
+• #script
+• #ping
+• #limit
+• #ceklimit
+• #speed
+• #status
+• #botstatus
+• #runtime
+
+❑ *GROUP MENU*
+• #kick
+• #add
+• #promote
+• #demote
+• #setname
+• #setdesc
+• #setppgrup
+• #tagall
+• #hidetag
+• #vote
+• #upvote
+• #devote
+• #cekvote
+• #delvote
+• #group
+• #editinfo
+• #antilink
+
+❑ *OWNER MENU*
+• #join
+• #leave
+• #getcase
+• #creategc
+• #block
+• #unblock
+• #bcall
+• #bcgc
+• #broadcast
+• #prem del 628xxxx
+• #prem add 628xxxx
+
+❑ *FUN MENU*
+• #jadian
+• #jodohku
+• #tictactoe
+• #delttt
+• #family100
+• #tebak
+• #math
+• #suitpvp
+
+❑ *CONVERT MENU*
+• #tahta
+• #teksto
+• #sticker
+• #toimage
+• #tovideo
+• #tomp3
+
+❑ *NULIS MENU*
+• #nulis
+• #nuliskiri
+• #nuliskanan
+• #foliokiri
+• #foliokanan
+
+❑ *ANIME MENU*
+• #nekonime
+• #randomloli
+• #randomwaifu
+• #randomneko
+• #randomanime
+
+❑ *DOWNLOAD MENU*
+• #play
+• #ytplay
+• #ytmp3
+• #ytmp4
+• #tiktok
+• #tiktokmp4
+• #tiktokmp3
+`
+}
